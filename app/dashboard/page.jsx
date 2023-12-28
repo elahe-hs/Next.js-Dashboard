05d89bf270c1c7049afada261435279e1a6e96d3
Elahe Hashemi <elahe.hs481@gmail.com>
2023-12-28 17:34:29 +0330

    navbar, cards, table and chart of the dashboard page created

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,23 @@
+import Card from '../components/dashboard/cart/card'
+import Transaction from '../components/dashboard/transaction/transaction'
+import Chart from '../components/dashboard/chart/chart'
+import RightSidebar from '../components/dashboard/rightSidebar/rightSidebar'
+import styles from './dashboard.module.scss'
+
 const Dashboard = () => {
-    return (<div>
-    dashboard page
+    return (<div  className={styles.wrapper}>
+        <div  className={styles.main}>
+    <div className={styles.cards}>
+        <Card />
+        <Card />
+        <Card />
+    </div>
+    <Transaction />
+    <Chart />
+    </div>
+    <div className={styles.side}>
+        <RightSidebar />
+    </div>
     </div>)
 }
 
